a18b34465bf3fdaeb8123968665b882e46f6298c
ktsuench <will289@hotmail.com>
2016-08-13 16:02:13 -0400

    added in slideshow to landing page

---
+++ public/js/scripts/landing.js
@@ -0,0 +1,12 @@
+/* Change this script so that it gets the name and count of images in the specified folder - done through call to nodejs backend*/
+var landing = document.getElementById('landing');
+var path = '../img/cover/';
+var cover = ['eng1', 'eng2', 'image center1', 'image center2', 'mac', 'slc1', 'slc2', 'TRSM'];
+var ext = '.jpg';
+var index = 1;
+
+//Switch the images every 10 seconds
+var imageSlideshow = window.setInterval(function() {
+	landing.setAttribute('style', 'background: url("' + path + cover[index++] + ext + '"); background-size: cover; background-position: 50% 80%');
+	index = index >= cover.length ? 0 : index;			
+}, 10000);
