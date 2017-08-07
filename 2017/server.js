5c2b955c46d8c1c1d23e62829bf70836c68e9691
ktsuench <will289@hotmail.com>
2017-08-06 22:30:54 -0400

    updated for recent changes

--- 2017/server.js
+++ 2017/server.js
@@ -1,6 +1,6 @@
 // .env loading ============================================
 // development purposes only
-if(!process.env.requireDotenv) require('dotenv').config();
+if(!process.env.requireDotenv) require('dotenv').config({path: "./2017/prod.env"});
 
 // modules =================================================
 var express         = require('express');
@@ -17,7 +17,7 @@ var crypto          = require('crypto');
 var db = require('./config/db');
 
 // set our port
-app.set('port', (process.env.PORT || 7000));
+app.set('port', (process.env.PORT_2017 || 7000));
 
 // connect to our postgres database
 var client = new pg.Client(db.url);
@@ -30,18 +30,18 @@ client.connect(function(err) {
 client.query("CREATE TABLE IF NOT EXISTS mailingList (id SERIAL PRIMARY KEY NOT NULL, email CHAR(100) NOT NULL);", function(err, result){
     if(err) throw err;
 
-    console.log('mailingList\n' + result.rows + '\n');
+    console.log('2017: mailingList');
 });
 
 // create userList table if it does not exist
 client.query("CREATE TABLE IF NOT EXISTS userList (id SERIAL PRIMARY KEY NOT NULL, username CHAR(50) NOT NULL, hash CHAR(100) NOT NULL);", function(err, result){
     if(err) throw err;
 
-    console.log('userList\n' + result.rows + '\n');
+    console.log('2017: userList');
 
     // create admin user if admin does not exist
     var hash = crypto.createHash('SHA256');
-    var pass = process.env.ADMINPASS || "hb=#}4v'd{f4kFS{";
+    var pass = process.env.ADMINPASS_2017 || "hb=#}4v'd{f4kFS{";
 
     pass = hash.update(pass).digest('base64');
 
@@ -49,7 +49,7 @@ client.query("CREATE TABLE IF NOT EXISTS userList (id SERIAL PRIMARY KEY NOT NUL
     client.query("UPDATE userList SET username='admin', hash='" + pass + "' WHERE username='admin';", function(err, result) {
         if(err) throw err;
 
-        console.log('checking if admin exists\n' + result.rows + '\n');
+        console.log('2017: checking if admin exists');
 
         // check number of updated records
         if(result.rowCount < 1) {
@@ -83,7 +83,7 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse cookies
-var key = process.env.COOKIE_SECRET || "gf'BTtV4E%^A%!/>s$rDk8#@cQqkV#739x+k";
+var key = process.env.COOKIE_SECRET_2017 || "gf'BTtV4E%^A%!/>s$rDk8#@cQqkV#739x+k";
 // refer to the express-session docs: https://github.com/expressjs/session
 app.use(session({path: '/2017', name: 'ruhacks', secret: key, httpOnly: true, secure: false, maxAge: null, resave: false, saveUninitialized: false}));
 
@@ -98,7 +98,7 @@ require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('2017: Node app is running on port', app.get('port'));
 });
 
 // expose app           
