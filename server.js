b9def5a69310b2ff3f0fb8dcd963c5a888e87342
ktsuench <will289@hotmail.com>
2017-08-08 00:29:22 -0400

    init 2018 dir

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ var app             = express();
 // configuration ===========================================
 
 // set our port
-app.set('port', (process.env.PORT || 7700));
+app.set('port', (process.env.PORT || 7000));
 app.use('/2017', require('./2017/server'));
+app.use('/', require('./2018/server'));
 
 // start app ===============================================
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('root: Node app is running on port', app.get('port'));
 });
 
-// expose app           
+// expose app
 exports = module.exports = app;
