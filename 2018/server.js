d33096cf0d7d5a43c2a1f2523cd1d324ab5908f5
ktsuench <will289@hotmail.com>
2017-09-17 15:30:18 -0400

    linted server file

--- 2018/server.js
+++ 2018/server.js
@@ -1,14 +1,18 @@
 // .env loading ============================================
 // development purposes only
-if(!process.env.hasOwnProperty("REQUIRE_DOT_ENV") || process.env.REQUIRE_DOT_ENV === "true") require('dotenv').config({path: "./2018/.env"});
+const dotenv = require('dotenv');
+
+if (!Object.prototype.hasOwnProperty.call(process.env, 'REQUIRE_DOT_ENV') || process.env.REQUIRE_DOT_ENV === 'true') {
+  dotenv.config({ path: './2018/.env' });
+}
 
 // modules =================================================
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var methodOverride = require('method-override');
-var crypto = require('crypto');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const methodOverride = require('method-override');
+
+const app = express();
 
 // configuration ===========================================
 
@@ -16,47 +20,45 @@ var crypto = require('crypto');
 app.set('port', (process.env.PORT_2018 || 7000));
 
 // get all data/stuff of the body (POST) parameters
-// parse application/json 
-app.use(bodyParser.json()); 
+// parse application/json
+app.use(bodyParser.json());
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse cookies
-var key = process.env.COOKIE_SECRET_2018 || "@\\Ks\\\"+#s}XA$c5@6PqJK-,7S+X5t^v^X&$[";
+const key = process.env.COOKIE_SECRET_2018 || '@\\Ks\\"+#s}XA$c5@6PqJK-,7S+X5t^v^X&$[';
 // refer to the express-session docs: https://github.com/expressjs/session
-app.use(session(
-  {
+app.use(session({
+  path: '/',
+  name: 'ruhacks-2018',
+  secret: key,
+  httpOnly: true,
+  secure: false,
+  maxAge: null,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
     path: '/',
-    name: 'ruhacks-2018',
-    secret: key,
-    httpOnly: true,
-    secure: false,
-    maxAge: null,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      path: '/'
-    }
-  }
-));
+  },
+}));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 // routes ==================================================
 require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('2018: Node app is running on port', app.get('port'));
 });
 
 // expose app
-exports = module.exports = app;
+module.exports = app;
