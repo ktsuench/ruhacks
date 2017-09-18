4c7208034c71d884a8cc8506fa189d17964231fb
ktsuench <will289@hotmail.com>
2017-09-18 01:08:55 -0400

    added in db models and schema
    needs testing

---
+++ 2018/app/db/startup.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const schema = require('./schema');
+const error = require('../../config/error');
+
+function getDbConnection(dbUrl) {
+  const connectionOptions = { useMongoClient: true, promiseLibrary: global.Promise };
+  let connection = null;
+
+  if (dbUrl) {
+    connection = mongoose.createConnection(dbUrl, connectionOptions, (err) => {
+      if (err) {
+        console.error(`[${err.name} ${err.code}]: ${err.message}\n${err.stack}`);
+      } else {
+        console.log('Connected to Mongoose DB');
+      }
+    });
+  } else {
+    throw new Error(error.noDbUrlProvided);
+  }
+
+  return connection;
+}
+
+function getModels(db) {
+  const Subscriber = db.model('Subscriber', mongoose.Schema(schema.subscriber));
+  const Hacker = db.model('Hacker', mongoose.Schema(schema.hacker));
+  const Volunteer = db.model('Volunteer', mongoose.Schema(schema.volunteer));
+
+  return {
+    Subscriber,
+    Hacker,
+    Volunteer,
+  };
+}
+
+module.exports = {
+  getDbConnection,
+  getModels,
+};
