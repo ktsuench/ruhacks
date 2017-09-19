7385ca5817a4f7a9757cc40914287a58c5b583fd
ktsuench <will289@hotmail.com>
2017-09-19 01:27:13 -0400

    added in insertion test for subscribers collection

--- 2018/tests/db.js
+++ 2018/tests/db.js
@@ -23,11 +23,40 @@ mocha.describe('Database', () => {
     });
   });
 
-  const models = db.getModels(db.getDbConnection(dbConfig.url));
+  const models = db.getModels(db.getDbConnection(dbConfig.default_url));
+
+  Object.keys(models).forEach((model) => {
+    models[model].remove({}, (err) => {
+      if (err) {
+        console.error(`[${err.name} ${err.code}]: ${err.message}\n${err.stack}`);
+      } else {
+        // console.log(`Emptied ${model} collection.`);
+      }
+    });
+  });
 
   mocha.describe('Subscriber Model', () => {
     mocha.it('Integrity Check', () => {
       Object.getPrototypeOf(models.Subscriber).should.equal(mongoose.Model);
+    });
+    mocha.it('Insert Bob', () => {
+      const subscriber = new models.Subscriber({
+        name: {
+          first: 'Bob',
+          last: 'Williams',
+        },
+        email: 'this.is@fake.email.addr',
+        gender: constants.gender[0],
+      });
+
+      return subscriber.save((err) => {
+        if (err) {
+          // console.error(`[${err.name} ${err.code}]: ${err.message}\n${err.stack}`);
+          return false;
+        }
+
+        return true;
+      });
     });
   });
   mocha.describe('Hacker Model', () => {
